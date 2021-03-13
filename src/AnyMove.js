adcd7ccbc9b821dc3acd5ea3c3729fb87a402135
Abe Binder <abebinder@gmail.com>
2021-03-13 14:40:21 -0800

    2 human players moving with validations

---
+++ src/AnyMove.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import * as Chess from "chess.js";
+import Chessboard from "chessboardjsx";
+
+class HumanVsHuman extends Component {
+
+    static propTypes = { children: PropTypes.func };
+
+    componentDidMount() {
+        this.game = new Chess();
+    }
+
+    state = {
+        fen: "start",
+        // square styles for active drop square
+        dropSquareStyle: {},
+        // custom square styles
+        squareStyles: {},
+        // square with the currently clicked piece
+        pieceSquare: "",
+        // currently clicked square
+        square: "",
+        // array of past game moves
+        history: []
+    };
+
+
+    onDrop = ({sourceSquare, targetSquare}) => {
+        // see if the move is legal
+        let move = this.game.move({
+            from: sourceSquare,
+            to: targetSquare,
+            promotion: "q" // always promote to a queen for example simplicity
+        });
+
+        // illegal move
+        if (move === null) return;
+        this.setState(({history, pieceSquare}) => ({
+            fen: this.game.fen(),
+            history: this.game.history({verbose: true}),
+        }));
+    };
+
+    render() {
+        const { fen, dropSquareStyle, squareStyles } = this.state;
+
+        return this.props.children({
+            position: fen,
+            onDrop: this.onDrop,
+        });
+    }
+}
+
+export default function AnyMove() {
+    return (
+        <div>
+            <HumanVsHuman>
+                {({
+                      position,
+                      onDrop,
+                      onMouseOverSquare,
+                      onMouseOutSquare,
+                      squareStyles,
+                      dropSquareStyle,
+                      onDragOverSquare,
+                      onSquareClick,
+                      onSquareRightClick
+                  }) => (
+                    <Chessboard
+                        id="humanVsHuman"
+                        width={320}
+                        position={position}
+                        onDrop={onDrop}
+                    />
+                )}
+            </HumanVsHuman>
+        </div>
+    );
+}
--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Chessboard from "chessboardjsx";
+import AnyMove from "./AnyMove";
 
 function App() {
   return (
-      <Chessboard position="start"/>
+      <AnyMove/>
   );
 }
 
