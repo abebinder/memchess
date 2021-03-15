c524a5a7a8aedcb3873bab3ed2766737afd49ad6
Abe Binder <abebinder@gmail.com>
2021-03-14 20:17:55 -0700

    cleaned up EcoLoader

--- src/EcoLoader.ts
+++ src/EcoLoader.ts
@@ -1,42 +1,28 @@
 
 import {ShortMove, Square} from "chess.js";
 import * as d3 from "d3";
-
-
-
+import { DSVRowArray, DSVRowString } from "d3";
 export class EcoLoader{
 
-
     public  load() : Map<string, ShortMove[]>{
-
         console.log("Loading tsv")
-        const my_map = new Map<string, ShortMove[]>();
-        const derp = d3.tsv("data/eco/a.tsv").then((data: any)=> {
-            console.log("data is ")
-            console.log(data)
+        const openingNameToShortMoveMap = new Map<string, ShortMove[]>();
+        d3.tsv("data/eco/a.tsv").then((data: DSVRowArray<string>)=> {
             for (const elem of data){
-                my_map.set(elem.name, this.createShortMoves(elem))
-            }
-        });
-
-
-
-        console.log(my_map)
-        return my_map;
+                openingNameToShortMoveMap.set(elem["name"] as string, this.createShortMoves(elem))
+            }});
+        console.log(openingNameToShortMoveMap)
+        return openingNameToShortMoveMap;
     }
 
-    createShortMoves(data: any): ShortMove[]{
-        var moves= data.moves.split(" ")
+    createShortMoves(data: DSVRowString): ShortMove[] {
+        var movesAsSpaceSeperatedString = data["moves"] as string;
+        var moves = movesAsSpaceSeperatedString.split(" ")
         let arr: ShortMove [] = [];
         for (const elem of moves) {
-            const shortMove = {from: elem.substring(0,2), to: elem.slice(-2)}
+            const shortMove = {from: elem.substring(0, 2) as Square, to: elem.slice(-2) as Square}
             arr.push(shortMove);
         }
         return arr;
     }
-
-
-
-
-
 }
