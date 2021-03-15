ec13a344400b6486bd533622fbd324d86e69d1a4
Abe Binder <abebinder@gmail.com>
2021-03-14 17:59:44 -0700

    builds a map

--- src/EcoLoader.ts
+++ src/EcoLoader.ts
@@ -6,15 +6,35 @@ import * as d3 from "d3";
 
 export class EcoLoader{
 
+
     public  load(tsvFilePath: string) : Map<string, ShortMove>{
 
         console.log("Loading tsv")
+
+        const my_map = new Map<any, any>();
+
+
         // @ts-ignore
-        const derp = d3.tsv("data/eco/a.tsv", function(data){
-            console.log(data)
+        const derp = d3.tsv("data/eco/a.tsv", (data: any) => {
+            my_map.set(data.name, this.createShortMoves(data))
         });
-        return new Map<string, ShortMove>();
+
+
+        console.log(my_map)
+        return my_map;
     }
+
+    createShortMoves(data: any): ShortMove[]{
+        var moves= data.moves.split(" ")
+        let arr: ShortMove [] = [];
+        for (const elem of moves) {
+            const shortMove = {from: elem.substring(0,2), to: elem.slice(-2)}
+            arr.push(shortMove);
+        }
+        return arr;
+    }
+
+
 
 
 
