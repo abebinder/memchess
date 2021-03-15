7eac5b09aa6cfbb624cc17b95d2de641445018f2
Abe Binder <abebinder@gmail.com>
2021-03-14 19:44:25 -0700

    resolved a type error in EcoLoader

--- src/EcoLoader.ts
+++ src/EcoLoader.ts
@@ -7,16 +7,16 @@ import * as d3 from "d3";
 export class EcoLoader{
 
 
-    public  load() : Map<string, ShortMove>{
+    public  load() : Map<string, ShortMove[]>{
 
         console.log("Loading tsv")
-
-        const my_map = new Map<any, any>();
-
-
-        // @ts-ignore
-        const derp = d3.tsv("data/eco/a.tsv", (data: any) => {
-            my_map.set(data.name, this.createShortMoves(data))
+        const my_map = new Map<string, ShortMove[]>();
+        const derp = d3.tsv("data/eco/a.tsv").then((data: any)=> {
+            console.log("data is ")
+            console.log(data)
+            for (const elem of data){
+                my_map.set(elem.name, this.createShortMoves(elem))
+            }
         });
 
 
