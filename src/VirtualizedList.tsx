7f796afebd11e68b8d05b0fb0148e217a12b4092
Abe Binder <abebinder@gmail.com>
2021-03-19 01:50:09 -0700

    converted VirtualizedList to typescript

--- src/VirtualizedList.tsx
+++ src/VirtualizedList.tsx
@@ -4,11 +4,12 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import { FixedSizeList } from 'react-window';
 
-class VirtualizedList extends React.Component {
+class VirtualizedList extends React.Component<{openings: string[]}> {
+
+    openings: string[] = [];
 
     constructor (props){
         super(props);
-        this.openings = []
         this.renderRow = this.renderRow.bind(this);
     }
 
@@ -24,7 +25,6 @@ class VirtualizedList extends React.Component {
     render() {
         if(this.openings.length === 0)  this.openings = this.props.openings
         return (
-            //TODO variable size list https://web.dev/virtualize-long-lists-react-window/
             <FixedSizeList height={600} width={300} itemSize={100} itemCount={this.openings.length}>
                 {this.renderRow}
             </FixedSizeList>
