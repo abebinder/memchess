65782007a5b6a9f0027197ec65739540ae379ecb
Abe Binder <abebinder@gmail.com>
2021-03-28 16:42:21 -0700

    Opening list highlights opening that is clicked

--- src/VirtualizedList.tsx
+++ src/VirtualizedList.tsx
@@ -1,35 +1,60 @@
-import React, {Component} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import { FixedSizeList } from 'react-window';
+import {FixedSizeList} from 'react-window';
 
-class VirtualizedList extends React.Component<{openings: string[]}> {
+interface VirtualizeListProps {
+    openings: string[]
+}
 
-    openings: string[] = [];
+interface VirtualizedListState {
+    selectedIndex: number
+}
 
-    constructor (props){
+class VirtualizedList extends React.Component<VirtualizeListProps, VirtualizedListState> {
+
+    constructor(props) {
         super(props);
+        //Only initialize state like this in constructor. Everywhere else use setstate https://stackoverflow.com/a/35548362
+        this.state = {
+            selectedIndex: 0,
+        }
         this.renderRow = this.renderRow.bind(this);
+    }
+
+
+    handleListItemClick(index) {
+        this.setState({
+            selectedIndex: index,
+        })
     }
 
     renderRow(props) {
         const {index, style} = props;
         return (
-            <ListItem button style={style} key={index}>
-                <ListItemText primary={`${this.openings[index]}`}/>
+            <ListItem
+                button style={style}
+                key={index}
+                selected={index === this.state.selectedIndex}
+                onClick={() => this.handleListItemClick(index)}
+            >
+                <ListItemText primary={`${this.props.openings[index]}`}/>
             </ListItem>
         );
     }
 
     render() {
-        if(this.openings.length === 0)  this.openings = this.props.openings
         return (
-            <FixedSizeList height={600} width={300} itemSize={100} itemCount={this.openings.length}>
+            <FixedSizeList height={600}
+                           width={300}
+                           itemSize={100}
+                           itemCount={this.props.openings.length}
+                           state={this.state}
+            >
                 {this.renderRow}
             </FixedSizeList>
         )
     }
 }
 
-export default  VirtualizedList;
+export default VirtualizedList;
