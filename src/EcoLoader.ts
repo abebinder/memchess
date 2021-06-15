84ee276b17a069af75ea3dc7ae34162071682a99
Abe Binder <abebinder@gmail.com>
2021-06-14 21:04:38 -1000

    created initialize method

--- src/EcoLoader.ts
+++ src/EcoLoader.ts
@@ -20,18 +20,64 @@ export interface OpeningNode{
 export class EcoLoader{
 
     prefixes: string[] = ['a', 'b', 'c', 'd', 'e']
+    idToNodeMap: Map<string, OpeningNode> = new Map<string, OpeningNode>()
+    rootNodes: OpeningNode[] = []
 
-    createShortMoves(data: DSVRowString): ShortMove[] {
-        var movesAsSpaceSeperatedString = data["moves"] as string;
-        var moves = movesAsSpaceSeperatedString.split(" ")
-        let arr: ShortMove [] = [];
-        for (const elem of moves) {
-            const shortMove = {from: elem.substring(0, 2) as Square, to: elem.slice(-2) as Square}
-            arr.push(shortMove);
-        }
-        return arr;
+
+
+
+    async initialize(){
+        let openingList = await this.createOpeningList();
+        this.idToNodeMap = await this.createIdToNodeMap(openingList);
+        this.rootNodes = await this.createRootNodes(this.idToNodeMap);
     }
 
+    createIdToNodeMap(openingList: Opening[]) {
+        let idToNodeMap = new Map<string, OpeningNode>();
+        for (const opening of openingList) {
+            let node: OpeningNode = {
+                id: this.unravel(opening.moves),
+                moves: opening.moves,
+                name: opening.name
+            }
+            idToNodeMap.set(node.id, node)
+        }
+        return idToNodeMap
+    }
+
+    private async createRootNodes(idToNodeMap: Map<string, OpeningNode>) {
+       let openingNodes = idToNodeMap.values();
+        let openingNodeChildMap = new Map<string, OpeningNode>();
+        let rootNodes: OpeningNode[] = []
+        for (const node of openingNodes) {
+            var addToMap = true;
+            for (let i = node.moves.length-1; i>-1; i--) {
+                if(i==0) {
+                    rootNodes.push(node)
+                    break;
+                }
+                const possibleParent = openingNodeChildMap.get(this.unravel(node.moves.slice(0, i)))
+                if(possibleParent){
+                    if(possibleParent.name === node.name){
+                        addToMap = false;
+                        break;
+                    }
+                    if(possibleParent.children) {
+                        possibleParent.children.push(node)}
+                    else{
+                        possibleParent.children = [node]
+                    }
+                    break;
+                }
+
+            }
+            if (addToMap) {
+                openingNodeChildMap.set(this.unravel(node.moves), node)
+            }
+        }
+        this.sortNodeList(rootNodes)
+        return rootNodes;
+    }
 
     public async loadMap() {
         let openingList = await this.createOpeningList();
@@ -74,6 +120,18 @@ export class EcoLoader{
         return {idToNodeMap: idToNodeMap, rootNodes: rootNodes};
     }
 
+    createShortMoves(data: DSVRowString): ShortMove[] {
+        var movesAsSpaceSeperatedString = data["moves"] as string;
+        var moves = movesAsSpaceSeperatedString.split(" ")
+        let arr: ShortMove [] = [];
+        for (const elem of moves) {
+            const shortMove = {from: elem.substring(0, 2) as Square, to: elem.slice(-2) as Square}
+            arr.push(shortMove);
+        }
+        return arr;
+    }
+
+
 
     private async createOpeningList() {
         let openingList: Opening[] = []
@@ -107,6 +165,5 @@ export class EcoLoader{
             if(openingNode.children) this.sortNodeList(openingNode.children)
         }
     }
-
 
 }
