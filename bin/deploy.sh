cc46c437dd38a2008322a63180722f6bbc065fe9
Abe Binder <abebinder@gmail.com>
2021-10-24 22:39:25 -0700

    a little closer

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,7 +3,14 @@ JOB_ID=$(aws amplify start-job --app-id $1 --branch-name $2 --job-type RELEASE |
 echo "Release started"
 echo "Job ID is $JOB_ID"
 
-while [[ "$(aws amplify get-job --app-id $1 --branch-name $2 --job-id $JOB_ID | jq -r '.job.summary.status')" =~ ^(PENDING|RUNNING)$ ]]; do sleep 1; done
+RESULTS=$(aws amplify get-job --app-id $1 --branch-name $2 --job-id $JOB_ID)
+echo $RESULTS
+while [[ "$(echo $RESULTS | jq -r '.job.summary.status')" =~ ^(PENDING|RUNNING)$ ]]
+do
+   sleep 1;
+   RESULTS=$(aws amplify get-job --app-id $1 --branch-name $2 --job-id $JOB_ID)
+done
+
 JOB_STATUS="$(aws amplify get-job --app-id $1 --branch-name $2 --job-id $JOB_ID | jq -r '.job.summary.status')"
 echo "Job finished"
 echo "Job status is $JOB_STATUS"
