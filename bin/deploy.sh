246185b6336be6f09ed599f83d06f514cdd54f96
Abe Binder <abebinder@gmail.com>
2021-10-25 17:03:51 -0700

    job says status

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,18 +3,19 @@ JOB_ID=$(aws amplify start-job --app-id $1 --branch-name $2 --job-type RELEASE |
 echo "Release started"
 echo "Job ID is $JOB_ID"
 
-RESULTS=$(aws amplify get-job --app-id $1 --branch-name $2 --job-id $JOB_ID)
-echo $RESULTS
-while [[ "$(echo $RESULTS | jq -r '.job.summary.status')" =~ ^(PENDING|RUNNING)$ ]]
-do
-   sleep 1;
-   RESULTS=$(aws amplify get-job --app-id $1 --branch-name $2 --job-id $JOB_ID)
-done
-
-JOB_STATUS="$(aws amplify get-job --app-id $1 --branch-name $2 --job-id $JOB_ID | jq -r '.job.summary.status')"
-echo "Job finished"
+#having condition at the end forces loop to be entered at least once
+while
+     sleep 1;
+     RESULTS=$(aws amplify get-job --app-id $1 --branch-name $2 --job-id $JOB_ID)
+     JOB_STATUS="$(echo $RESULTS | jq -r '.job.summary.status')"
+     echo "Job status is $JOB_STATUS"
+     [[ "$JOB_STATUS" =~ ^(PENDING|RUNNING|CANCELLING)$ ]]
+do true; done
+echo "Job finished, results:"
+echo "$RESULTS"
 echo "Job status is $JOB_STATUS"
 if [ "$JOB_STATUS" != "SUCCEED" ]; then
     echo "Deployment did not succeed"
     exit 1
 fi
+echo "Deployment succeeded"
